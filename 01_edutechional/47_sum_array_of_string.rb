68aad9a7d800d1a88c45e239f3eff079e0df205f
Eugene Makarikhin <contact@aboutslack.info>
2024-11-22 14:53:18 +0100

    Added edutechional/47_sum_array_of_string

---
+++ 01_edutechional/47_sum_array_of_string.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'rspec'
+
+# This guide examines an efficient process for summing an array filled with string based integers in Ruby.
+#
+# https://www.youtube.com/watch?v=4OIqLdTFtbQ&list=PLgYiyoyNPrv_UhCfDLOT8yZPmVeOFjqGH&index=51
+
+def string_sum(nums)
+  nums.map(&:to_i).sum
+end
+
+p string_sum([1, 2, 3])
+
+describe '#string_sum' do
+  it 'sums an array of string based integers' do
+    expect(string_sum('1'..'20')).to eq(210)
+    expect(string_sum('100'..'1000')).to eq(495_550)
+  end
+
+  it 'sum an array of integers' do
+    expect(string_sum(100..1000)).to eq(495_550)
+  end
+end
