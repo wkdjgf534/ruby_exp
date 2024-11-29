9a6f6dc6f51d5ce4b8855caf47cbc71e241e7094
Eugene Makarikhin <contact@aboutslack.info>
2024-11-29 23:46:43 +0100

    Added edutechional/60_validate_length_in_array

---
+++ 01_edutechional/60_validate_length_in_array.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rspec'
+
+# This guide explains how to validate the length of every element in an Array in Ruby.
+#
+# https://www.youtube.com/watch?v=Slm3_kB1y6o&list=PLgYiyoyNPrv_UhCfDLOT8yZPmVeOFjqGH&index=64
+
+arr_one = %w[words that have size requirements]
+arr_two = %w[words that nave not all 4 chatacters long]
+
+# Standard Ruby Array class
+class Array
+  def at_least_four_characters? = all? { |elem| elem.length >= 4 }
+end
+
+p arr_one.at_least_four_characters?
+p arr_two.at_least_four_characters?
+
+describe '.at_least_four_characters?' do
+  it 'checks if an array contains elements that atr all at least 4 characters long' do
+    expect(arr_one.at_least_four_characters?).to be_truthy
+    expect(arr_two.at_least_four_characters?).to be_falsey
+  end
+end
