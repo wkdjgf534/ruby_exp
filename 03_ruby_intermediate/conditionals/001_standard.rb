6836fb4d9520dc28fa6e0ac3e4596ae885ad083c
Eugene Makarikhin <contact@aboutslack.info>
2025-11-22 14:13:42 +0100

    Added 03_ruby_intermediate/conditionals/001_standard

---
+++ 03_ruby_intermediate/conditionals/001_standard.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+str = 'My great string'
+
+puts 'Something else' if str == 'My great string'
+
+grade = 100
+
+if grade >= 90
+  puts 'Congratulations Honor Student'
+else
+  puts 'work harder'
+end
