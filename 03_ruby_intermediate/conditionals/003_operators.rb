6872f996cefc0fa0f90a88c02ff9c0668dc58b00
Eugene Makarikhin <contact@aboutslack.info>
2025-11-22 20:51:32 +0100

    Added 03_ruby_intermediate/conditionals/003_operators

---
+++ 03_ruby_intermediate/conditionals/003_operators.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+x = 1
+y = 2
+
+puts x == y
+puts x != y
+puts x > y
+puts x < y
+puts x >= y
+puts x <= y
+puts x <=> y
+puts x === y
+puts x.eql?(y)
+puts x.equal?(y)
